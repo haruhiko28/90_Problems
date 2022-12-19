d307378806981942bc4d88e6752a3b995637bc5b
haruhiko28 <not.hisano.soccer@gmail.com>
2022-12-19 22:17:26 +0900

    add 055

--- 055/Main.java
+++ 055/Main.java
@@ -8,6 +8,28 @@ public class Main {
         int P = sc.nextInt();
         int Q = sc.nextInt();
 
-        
+        int[] A = new int[N];
+        for(int i=0; i<N; i++) {
+           A[i] = sc.nextInt();
+        }
+        int ans = 0;
+        for (int i=0;i<N;i++){
+            long a = A[i] %P;
+            for (int j=0;j<i;j++){
+                long b = a *A[j] %P;
+                for (int k=0;k<j;k++){
+                    long c = b *A[k] %P;
+                    for (int l=0;l<k;l++) {
+                        long d = c *A[l] %P;
+                        for (int m=0;m<l;m++){
+                            long e = d *A[m] %P;
+                            if (e == Q)
+                            ++ans;
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println(ans);
     }
 }
