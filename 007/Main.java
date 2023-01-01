d8079e5e53d3d49ebc591f327ba67106cb82be79
haruhiko28 <not.hisano.soccer@gmail.com>
2023-01-02 01:16:15 +0900

    add 0101

---
+++ 007/Main.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 入力
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        for(int i=0;i<N;i++){
+            A[i] = sc.nextInt();
+        }
+        
+        // Aをソート
+        Arrays.sort(A);
+        
+        int Q = sc.nextInt();
+        for(int j=0;j<Q;j++){
+            int B = sc.nextInt();
+        }
+        
+
+    }    
+}
