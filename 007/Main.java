b73242d4f0dac1b6dca033702babac97d8d5e1c1
haruhiko28 <not.hisano.soccer@gmail.com>
2023-01-02 11:08:02 +0900

    add 007

--- 007/Main.java
+++ 007/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -15,10 +16,38 @@ public class Main {
         Arrays.sort(A);
         
         int Q = sc.nextInt();
+
+        ArrayList<Double> ans = new ArrayList<Double>();
+        
         for(int j=0;j<Q;j++){
             int B = sc.nextInt();
-        }
-        
 
+            double left = -1;
+            double right = N;
+
+            while ((right - left) > 1) {
+                double mid = Math.floor((left + right) / 2);
+                if (A[(int)mid] >= B){
+                    right = mid;
+                }else{
+                    left = mid;
+                }
+            }
+
+            double comp1 = Math.pow(10, 9);
+            double comp2 = Math.pow(10, 9);
+
+            if(left >= 0){
+                comp1 = Math.abs(A[(int)left] -B);
+            }
+            if (left <N-1){
+                comp2 = Math.abs(A[(int)left+1] -B);
+            }
+            ans.add(Math.min(comp1,comp2));
+        }
+
+        for (double a:ans){
+            System.out.println((int)a);
+        }
     }    
 }
