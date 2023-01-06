9f47138830acda4ca75660cc4d7a0bbfe3ec78c7
haruhiko28 <not.hisano.soccer@gmail.com>
2023-01-06 23:57:15 +0900

    20220106

---
+++ 018/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int l = sc.nextInt();
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+        int q = sc.nextInt();
+
+        
+    }    
+}
