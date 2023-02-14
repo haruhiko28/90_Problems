a0578b65644556d1786b4763dac873f2e5780754
haruhiko28 <not.hisano.soccer@gmail.com>
2023-02-15 01:40:33 +0900

    add 082

--- 082/Main.java
+++ 082/Main.java
@@ -6,14 +6,32 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         long L = sc.nextLong();
         long R = sc.nextLong();
-        long D = 1000000007L;
         sc.close();
+
+        int mod = 1000000007;
         long ans = 0;
-        for (long i=L;i<R+1;i++){
-            int valLen = String.valueOf(i).length(); 
-            ans += valLen * i;
-            ans %= D;
-        }
+        ans = fnc(L, R, mod);
         System.out.println(ans);
+    }
+
+    public static long fnc(long l, long r, int mod){
+        long count = 0;
+        long l_len = String.valueOf(l).length(); 
+        long r_len = String.valueOf(r).length(); 
+
+        for (long i=l_len;i<=r_len;i++){
+            long x = Math.max(l, (long)Math.pow(10,i-1));
+            long y = Math.min(r, (long)Math.pow(10,i)-1);
+            long ans = (((i*((y-x+1)%mod))%mod)*((x+y)%mod)) %mod;
+            
+            if(ans%2==0){
+                ans = ans/2;
+            }else{
+                ans = (ans + mod)/2;
+            }
+            count += ans;
+            count = count % mod;
+        }
+        return count;
     }
 }
